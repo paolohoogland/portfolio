78d9538b03775f13a93d003f239d2cf5302cb496
phoogland <paolo.hoogland@etu.unistra.fr>
2025-06-26 12:48:02 +0200

    Started css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,13 @@ const About = () => {
                 <h2>about_</h2>
             </div>
             <div className="sectionBody">
-                <p>I'm currently a Software Developer at <a href="https://www.al-enterprise.com/fr-fr">Alcatel-Lucent Enterprise</a>, working in the Backend team. I'm developing a load and performance testing interface.</p>
-                <p>I hold a Bachelor's degree in Computer Science with a specialization in Application Development: design, development, and testing, from IUT Robert Schuman in Strasbourg. I am currently pursuing an engineering degree at the National Conservatory of Arts and Crafts (CNAM), Strasbourg.</p>
+                <div id="info">
+                    <p>I'm currently a Software Developer at <a href="https://www.al-enterprise.com/fr-fr">Alcatel-Lucent Enterprise</a>, working in the Backend team. I'm developing a load and performance testing interface.</p>
+                    <p>I hold a Bachelor's degree in Computer Science with a specialization in Application Development: design, development, and testing, from <a href="https://iutrs.unistra.fr">IUT Robert Schuman</a> in Strasbourg. I am currently pursuing an engineering degree at the <a href="https://www.cnam-grandest.fr">National Conservatory of Arts and Crafts (CNAM)</a>, Strasbourg.</p>
+                </div>
+                <div id="myPic">
+                    <img src="../src/assets/img/smile.jpeg" alt="Paolo Hoogland" />
+                </div>
             </div>
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,21 @@
 const Navbar = () => {
     return (
         <nav>
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#experience">Experience</a>
-            <a href="#projects">Projects</a>
+            <div>
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#experience">Experience</a>
+                <a href="#projects">Projects</a>
+            </div>
             <span></span>
-            <a href="https://github.com/paolohoogland">
-                <img src="../src/assets/img/github.png" alt="GitHub" />
-            </a>
-            <a href="https://www.linkedin.com/in/paolohoogland/">
-                <img src="../src/assets/img/linkedin.png" alt="LinkedIn" />
-            </a>
+            <div>
+                <a href="https://github.com/paolohoogland">
+                    <img src="../src/assets/img/github.png" alt="GitHub" />
+                </a>
+                <a href="https://www.linkedin.com/in/paolohoogland/">
+                    <img src="../src/assets/img/linkedin.png" alt="LinkedIn" />
+                </a>
+            </div>
         </nav>
     )
 }
